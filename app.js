536d634c4760775d813158cd3a9a78305723b6e3
Patrick Terry <dpatrickterry@gmail.com>
2014-08-29 14:14:09 -0700

    update

--- app.js
+++ app.js
@@ -23,5 +23,7 @@ app.get('/example', function (req, res) {
 
 
 
-app.listen(3000);
-console.log( 'Listening on port 3000' );
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
