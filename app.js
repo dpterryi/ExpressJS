dff2e4910cf2ebad200c8bdfc058fb6d4a94f9e8
Patrick Terry <dpatrickterry@gmail.com>
2014-08-30 13:09:14 -0700

    update

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var path = require('path');
-//var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express'),
+    morgan  = require( 'morgan' ),
+    path    = require('path'),
+    exphbs  = require('express-handlebars');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-//app.use(favicon());
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -22,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/example', example);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
